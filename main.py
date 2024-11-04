381c315208d3b543f9d905762fabca0a5afd205b
PoonamKalita18 <arunakalita2015@gmail.com>
2024-11-04 21:46:01 +0530

    sdrg

---
+++ main.py
@@ -0,0 +1,11 @@
+#creating a list of 5 numbers
+L = [10,20,30,40,50]
+
+#input given by the user
+N=int(input('enter a number'))
+
+#numbers guessing
+if N==20:
+    print('You won the game')
+else:
+    print('you loss the game')
